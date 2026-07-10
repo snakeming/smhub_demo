c2370a7a99c87a00a06691d3d2acea0b2a55db72
Cliff <snakeming@gmail.com>
2026-07-10 11:35:58 +0800

    build 2026-07-10_11-35-58

---
+++ deployment_config/miinternal-dev-a.js
@@ -0,0 +1,5 @@
+window.deploymentConfig = {
+  PRE_LOGIN_CONFIG_BASE_URL: 'https://miinternal-dev-a.milely.net/api/Milely/',
+  DEPLOYMENT_KEY: 'miinternal-dev-a',
+  APP_ENV: 'dev'
+};
---
+++ deployment_config/milocal-dev.js
@@ -0,0 +1,5 @@
+window.deploymentConfig = {
+  PRE_LOGIN_CONFIG_BASE_URL: 'https://127.0.0.1:8000/api/Milely/',
+  DEPLOYMENT_KEY: 'milocal-dev',
+  APP_ENV: 'dev'
+};
